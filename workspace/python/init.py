916446621a447d7235cc1035856789583c37fe6d
m-hiko <ana@m-hiko.info>
2021-04-04 12:17:47 +0900

    dynamodbを追加

---
+++ workspace/python/init.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main():
+    print("Hello MAIN!")
+
+    
+def yobidasi():
+    print("Hello yobidasi!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ workspace/python/init2.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import init
+
+init.yobidasi()
